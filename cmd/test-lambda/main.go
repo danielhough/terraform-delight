7dd8291f1fd0d49fa5cfd72c1defb3e309742161
Daniel Hough <22590796+danielhough@users.noreply.github.com>
2024-02-11 01:07:04 +0000

    Formatting

--- cmd/test-lambda/main.go
+++ cmd/test-lambda/main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "net/http"
+	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"go.uber.org/zap"
 
-    "github.com/danielhough/terraform-delight/internal/logger"
+	"github.com/danielhough/terraform-delight/internal/logger"
 )
 
 type LambdaAPIGatewayHandler func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
 
 type Response struct {
-    Message string `json:"message"`
+	Message string `json:"message"`
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var err error
 
-    zapLogger, err := logger.Production()
-    if err != nil {
-        log.Fatal("failed to initialise logger", err)
-    }
+	zapLogger, err := logger.Production()
+	if err != nil {
+		log.Fatal("failed to initialise logger", err)
+	}
 
-    zapLogger.Info("Testing logging")
+	zapLogger.Info("Testing logging")
 
-    body, err := json.Marshal(Response{Message: "Testing Lambda Response"})
-    if err != nil {
-        zapLogger.Error("failed to marshal response", zap.Error(err))
-        return events.APIGatewayProxyResponse{}, err
-    }
+	body, err := json.Marshal(Response{Message: "Testing Lambda Response"})
+	if err != nil {
+		zapLogger.Error("failed to marshal response", zap.Error(err))
+		return events.APIGatewayProxyResponse{}, err
+	}
 
-    return events.APIGatewayProxyResponse{
-        Body:       string(body),
-        Headers:    map[string]string{"Content-Type": "application/json"},
-        StatusCode: http.StatusOK,
-    }, nil
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		StatusCode: http.StatusOK,
+	}, nil
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
